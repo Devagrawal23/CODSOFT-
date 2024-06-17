13e887b020c2c187b9e0913821226f16bdcce4a3
Deven Agrawal <devagrawal345@gmail.com>
2024-06-17 17:59:33 +0530

    add files

---
+++ calculator/script.js
@@ -0,0 +1,19 @@
+let result = document.getElementById('result');
+
+function insertValue(value){
+    result.value +=  value;
+
+}
+function clearResult(){
+    result.value ='';
+}
+function deleteResult(){
+    result.value = result.value.slice(0, -1);
+}
+function  calculate(){
+    try{
+result.value = eval(result.value);
+    }catch{
+result.value = "error";
+    }
+}
